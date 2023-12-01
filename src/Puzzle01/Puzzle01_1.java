6d6aa01e0adf3f36afd587d146b4ebec61a14a50
yohnny <114059682+SmolYohnny@users.noreply.github.com>
2023-12-01 11:31:32 +0100

    foldering

--- src/Puzzle01/Puzzle01_1.java
+++ src/Puzzle01/Puzzle01_1.java
@@ -1,3 +1,5 @@
+package Puzzle01;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
--- src/Puzzle01/Puzzle01_2.java
+++ src/Puzzle01/Puzzle01_2.java
@@ -1,3 +1,5 @@
+package Puzzle01;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
