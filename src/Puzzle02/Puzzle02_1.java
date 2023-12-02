955cc1eacf441fea7b39d8d2cdb84483ba64df9d
yohnny <114059682+SmolYohnny@users.noreply.github.com>
2023-12-02 15:58:14 +0100

    test working, input doesnt

--- src/Puzzle02/Puzzle02_1.java
+++ src/Puzzle02/Puzzle02_1.java
@@ -22,22 +22,20 @@ public class Puzzle02_1 {
                 for (int i = 0; i < noSemi.length; i++) {
                     String[] ind = noSemi[i].split(", ");
                     for (int j = 0; j < ind.length; j++) {
-                        if (ind[j].contains("red")) r = Integer.parseInt(ind[j].replace(" red", ""));
+                        if (ind[j].contains("red") && r < Integer.parseInt(ind[j].replace(" red", ""))) r = Integer.parseInt(ind[j].replace(" red", ""));
                         //System.out.println(r);
-                        if (ind[j].contains("green")) g = Integer.parseInt(ind[j].replace(" green", ""));
+                        if (ind[j].contains("green") && g < Integer.parseInt(ind[j].replace(" green", ""))) g = Integer.parseInt(ind[j].replace(" green", ""));
                         //System.out.println(g);
-                        if (ind[j].contains("blue")) b = Integer.parseInt(ind[j].replace(" blue", ""));
+                        if (ind[j].contains("blue") && b < Integer.parseInt(ind[j].replace(" blue", ""))) b = Integer.parseInt(ind[j].replace(" blue", ""));
                         //System.out.println(b);
                     }
 
                 }
-                System.out.println(noIDs[0]);
                 if (r < 12 && g < 13 && b < 14) {
                     finalResult += Integer.parseInt(noIDs[0]);
                 }
-                System.out.println(finalResult);
-
             }
+            System.out.println(finalResult);
         }
 
 
