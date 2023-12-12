4595aa05d384faf0cd0f57478bf6041584584c77
yohnny <114059682+SmolYohnny@users.noreply.github.com>
2023-12-12 12:25:12 +0100

    day 7 base

---
+++ src/Puzzle07/Puzzle07.java
@@ -0,0 +1,7 @@
+package Puzzle07;
+
+public class Puzzle07 {
+    public static void main(String[] args) {
+        //https://stackoverflow.com/questions/15452429/java-arrays-sort-2d-array
+    }
+}
